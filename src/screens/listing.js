7a5dc52afd8892e5a057a78869d2e220bd499349
Maryam-hub455 <mehboobahmeddhudhi786@gmail.com>
2025-06-28 16:28:22 +0500

    Emoji listing

--- src/screens/listing.js
+++ src/screens/listing.js
@@ -3,35 +3,22 @@ import React from 'react';
 import { View, ImageBackground, StyleSheet,ScrollView,Text } from 'react-native';
 
 const ListingScreen = () => {
-  const data = [1,2,3,4,5,6,7,8,9,10];
+  const data = ["😋","🙄","😊","❤","😪","😴","😑","😎"];
   return (
     <ScrollView>
-      <ScrollView horizontal={true}>
+      
       {data.map((item) => (
       
-      <ImageBackground 
-      source={{uri:"https://cdn.pixabay.com/photo/2015/01/07/10/24/aster-591311_1280.jpg"}} 
-      style={styles.image} >
+      
 
         <Text style={styles.textStyle}>{item}</Text>
-      </ImageBackground>
-      
-      ))};
-    </ScrollView>
-      {data.map((item) => (
-      
-      <ImageBackground 
-      source={{uri:"https://cdn.pixabay.com/photo/2025/02/20/07/51/ai-generated-9419220_1280.png"}} 
-      style={styles.image} >
-
-        <Text style={styles.textStyle}>{item}</Text>
-      </ImageBackground>
-      
-      ))};
     
+      
+      ))};
+  
     </ScrollView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
