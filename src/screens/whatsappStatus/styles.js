3316e034597e54dbfb4b6f844faa5f622f9e12ea
Maryam-hub455 <mehboobahmeddhudhi786@gmail.com>
2025-07-01 22:57:17 +0500

    lecture 11 whatsappStatusApp

--- src/screens/whatsappStatus/styles.js
+++ src/screens/whatsappStatus/styles.js
@@ -1,32 +1,54 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  
   header: {
-    marginTop: 50,
-    paddingHorizontal:50,
-  
+    marginTop: 60,
+    paddingHorizontal: 10,
   },
-  headerText:{
-  fontSize:40,
-  fontWeight:"bold",
-  },
-  statusListing:{
 
+  headerText: {
+    fontSize: 40,
+    fontWeight: "bold",
+     
   },
-  statusListingHeading:{
-    flexDirection:"row",
-    justifyContent:"space-between",
+
+  statusListing: {
   },
-  headerSubListingContainer:{
-    flexDirection:"row",
-    gap:10,
+
+  statusListingHeading: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     
   },
-  headerSubText:{
-      fontSize:30,
-      fontWeight:"bold",
-      paddingHorizontal:10
-    },
-  
+  headerIcon:{
+    backgroundColor:"#f6f5f4",
+    height:40,
+    width:40,
+    borderRadius:20,
+    justifyContent:"center",
+    alignItems:"bold",
+ 
+  },
+
+  headerSubIconContainer: {
+    flexDirection: "row",
+    gap: 10,
+     
+  },
+
+  headerSubText: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  statusContainer: {
+    alignItems: "center",
+    gap: 10,
+    marginVertical: 10,
+    backgroundColor: "#f0f0f0",
+    height: 150,
+    width: 100,
+    borderRadius: 10,
+    justifyContent: "center",
+    marginRight: 10,
+  }
 });
--- src/screens/whatsappStatus/whatsappStatus.js
+++ src/screens/whatsappStatus/whatsappStatus.js
@@ -1,39 +1,60 @@
-import { View, Text,StyleSheet } from 'react-native'
-import React from 'react'
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import {styles} from './styles';
-import { IconContainer } from '../../components/iconContainer';
+import { FlatList, Text, View } from "react-native";
+import React from "react";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import Entypo from "@expo/vector-icons/Entypo";
+
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+import { styles } from "./styles";
+import { IconContainer } from "../../components/iconContainer";
+
 export default function WhatsappStatus() {
+  const numericData = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  const _renderStatus = (item) => {
+    return (
+      <View style={styles.statusContainer}>
+        <Text>{item}</Text>
+      </View>
+    );
+  };
+
   return (
     <View>
-      <View style={StyleSheet.header}>
+      <View style={styles.header}>
         <IconContainer
-        icon={<MaterialCommunityIcons
-         name="dots-horizontal" 
-         size={40} 
-         color="black" />}
-        />
-        
-         </View>
-         <Text style={styles.headerText}>Updates</Text>
-         <View style={styles.statusListing}>
-         <View style={styles.statusListingHeading}>
-
-          <Text style={styles.headerSubText}>Status</Text>
-          
-            <IconContainer
-              icon={<FontAwesome name="camera" size={24} color="black" />}
+          icon={
+            <MaterialCommunityIcons
+              name="dots-horizontal"
+              size={35}
+              color="black"
             />
-              
-            <IconContainer
-            icon={<Ionicons name="pencil" size={24} color="black" />}
-             />
-            
-             </View>
+          }
+        />
+        <Text style={styles.headerText}>Updates</Text>
+
+        <View style={styles.statusListing}>
+          <View style={styles.statusListingHeading}>
+            <Text style={styles.headerSubText}>Status</Text>
+            <View style={styles.headerSubIconContainer}>
+              <IconContainer
+                icon={<Entypo name="camera" size={24} color="black" />}
+              />
+              <IconContainer
+                icon={<Ionicons name="pencil" size={24} color="black" />}
+              />
+            </View>
           </View>
-         </View>
-      
-  )
+
+          <FlatList
+            data={numericData}
+            horizontal={true}
+            renderItem={({ item }) => _renderStatus(item)}
+          />
+
+          {/* list banani h is ma */}
+        </View>
+      </View>
+    </View>
+  );
 }
