20fd169fc9bf0442f1a1ad7ec56f30e8270d2b99
Maryam-hub455 <mehboobahmeddhudhi786@gmail.com>
2025-07-17 14:29:44 +0500

    part lec 14

--- src/screens/practiceMedia.js
+++ src/screens/practiceMedia.js
@@ -1,6 +1,13 @@
-import { Button, StyleSheet, Image, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Image,
+  View,
+  TouchableOpacity,
+} from "react-native";
 import React, { useState } from "react";
 import { launchCameraAsync } from "expo-image-picker";
+import Icon from "@expo/vector-icons/MaterialIcons";
 export default function PracticeMedia() {
   const [image, setImage] = useState();
   async function onCameraPress() {
@@ -11,10 +18,18 @@ export default function PracticeMedia() {
       setImage(result.assets[0].uri);
     }
   }
+
+  function onCrossPressed() {
+    setImage(null);
+  }
   return (
     <View style={styles.container}>
-      <Button title="Camera" onPress={onCameraPress} />
-      <Image source={{ uri: image }} style={styles.image} />
+      <TouchableOpacity onPress={onCameraPress}>
+        <Image source={{ uri: image }} style={styles.image} />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={onCrossPressed} style={styles.imageCancel}>
+        <Icon name="cancel" size={50} color="red\" />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -30,5 +45,12 @@ const styles = StyleSheet.create({
   image: {
     width: 200,
     height: 200,
+    borderRadius: 100,
+    borderWidth: 2,
+  },
+  imageCancelIcon: {
+    marginTop: 200,
+    marginRight:200,
+
   },
 });
